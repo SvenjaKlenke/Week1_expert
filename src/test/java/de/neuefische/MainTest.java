f3dcc87b7c4f6697df938b1a0c18ae5e1b10eaf2
svenjaklenke <svenja.klenke@gmx.de>
2023-04-23 19:19:14 +0200

    Add tests

---
+++ src/test/java/de/neuefische/MainTest.java
@@ -0,0 +1,96 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void roemischeZahl_1() {
+        //GIVEN
+        int a = 1;
+        String b = "I";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_5() {
+        //GIVEN
+        int a = 5;
+        String b = "V";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_10() {
+        //GIVEN
+        int a = 10;
+        String b = "X";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_50() {
+        //GIVEN
+        int a = 50;
+        String b = "L";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_100() {
+        //GIVEN
+        int a = 100;
+        String b = "C";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_3() {
+        //GIVEN
+        int a = 3;
+        String b = "III";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_4() {
+        //GIVEN
+        int a = 4;
+        String b = "IV";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+    @Test
+    void roemischeZahl_14() {
+        //GIVEN
+        int a = 14;
+        String b = "XIV";
+        //WHEN
+        String actual = Main.roemischeZahl(a);
+        //THEN
+        assertEquals(b, actual);
+    }
+
+}
